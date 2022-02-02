6b4e530f354accf96e2f987651f00888247438ed
Senén <senen.marques@live.u-tad.com>
2022-02-02 09:58:18 +0100

    finished Process class

--- src/Process.java
+++ src/Process.java
@@ -2,11 +2,17 @@
 public class Process {
 	
 	private int arrival;
-	private int time;
+	private int t;
+	private int start;
+	private int end;
+	private int totalT;
+	private int wait;
+	private double penalty;
 	
-	Process(int arrival, int time) {
+	Process(int arrival, int t) {
 		this.arrival = arrival;
-		this.time = time;
+		this.t = t;
+		this.penalty = 0;
 	}
 	
 	void setArrival(int arrival) {
@@ -17,14 +23,52 @@ public class Process {
 		return arrival;
 	}
 	
-	void setTime(int time) {
-		this.time = time;
+	void setTime(int t) {
+		this.t = t;
 	}
 	
 	int getTime() {
-		return time;
+		return t;
 	}
 	
+	void setStart(int start) {
+		this.start = start;
+	}
 	
+	int getStart() {
+		return start;
+	}
+	
+	void setEnd(int end) {
+		this.end = end;
+	}
+	
+	int getEnd() {
+		return end;
+	}
+	
+	void setTotalT(int totalT) {
+		this.totalT = totalT;
+	}
+	
+	int getTotalT() {
+		return totalT;
+	}
+
+	void setWait(int wait) {
+		this.wait = wait;
+	}
+	
+	int getWait() {
+		return wait;
+	}
+	
+	void setPenalty() {
+		penalty = totalT / t;
+	}
+	
+	double getPenalty() {
+		return penalty;
+	}
 
 }
