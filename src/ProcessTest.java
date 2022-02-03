3066edd82b0c7e000a7930eb2ed106d26bf18ef0
Senén <senen.marques@live.u-tad.com>
2022-02-03 23:33:51 +0100

    eclipse was sperging out

--- src/ProcessTest.java
+++ src/ProcessTest.java
@@ -33,8 +33,8 @@ public class ProcessTest {
 	// ask user for time of process and return it
 	static int getTime() {
 		System.out.print("Introduzca tiempo del proceso: ");
-		int t = in.nextInt();
-		return t;
+		int time = in.nextInt();
+		return time;
 	}
 
 	public static void main(String[] args) {
--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -5,13 +5,8 @@ public class RoundRobin extends Process {
 	int quantum;
 	ArrayList<Integer> remainders = new ArrayList<>();
 	
-	RoundRobin(int arrival, int time) {
+	public RoundRobin(int arrival, int time) {
 		super(arrival, time);
-	}
-	
-	RoundRobin(int arrival, int time, int quantum) {
-		super(arrival, time);
-		this.quantum = quantum;
 	}
 	
 	void setStart(int[] starts, int[] ends) {
