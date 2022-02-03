92589adcf41ab2030a5ce5223cc16027fa816e1f
Senén <senen.marques@live.u-tad.com>
2022-02-03 23:23:24 +0100

    another commit

--- src/RoundRobin.java
+++ src/RoundRobin.java
@@ -1,11 +1,20 @@
+import java.util.ArrayList;
 
 public class RoundRobin extends Process {
+	
+	int quantum;
+	ArrayList<Integer> remainders = new ArrayList<>();
 	
 	RoundRobin(int arrival, int time) {
 		super(arrival, time);
 	}
 	
-	void setStart() {
+	RoundRobin(int arrival, int time, int quantum) {
+		super(arrival, time);
+		this.quantum = quantum;
+	}
+	
+	void setStart(int[] starts, int[] ends) {
 		// write your code here
 	}
 	
